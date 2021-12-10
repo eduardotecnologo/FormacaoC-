5e51db9ff506c691cf1a3bebb550b768f5bf001e
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-12-09 22:12:31 -0300

    Contas com doubles

--- JogoAdivinha/index.cpp
+++ JogoAdivinha/index.cpp
@@ -5,25 +5,43 @@ int main(){
     
     cout <<"*****************************************" << endl;
     cout <<"**   Bem vindo ao Jogo da Adivinhação  **" << endl;
-    cout <<"*****************************************" << endl;   
+    cout <<"**     Você tem 1000 pontos manolo     **" << endl;
+    cout <<"*****************************************" << endl; 
 
     const int NUMERICSECRET = 42;
+    bool error = true;
+    int tentativas = 0;
+    double pontos = 1000.0;
 
-    int chute;
-    cout << "Qual o seu chute? " << endl;
+    while( error){
+        tentativas++;
 
-    cin >> chute;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
 
-    cout << "O valor do seu chute é: " << chute << endl;
+        cout << "Qual o seu chute? " << endl;
 
-    bool acertou = chute == NUMERICSECRET;
-    bool maior = chute > NUMERICSECRET;
+        cin >> chute;
 
-    if(acertou){
-        cout << "Parabéns! Você acertou o número secreto!" << endl;
-    }else if(maior){
-        cout << "Ops! Seu chute foi maior que o número secreto" << endl;
-    }else{
-        cout << "Ops! Seu chute foi menor que o número secreto" << endl;
+        double pontosperdidos = abs(chute - NUMERICSECRET) / 2.0;
+        pontos = pontos - pontosperdidos;
+
+        cout << "O valor do seu chute é: " << chute << endl;
+        bool acertou = chute == NUMERICSECRET;
+        bool maior = chute > NUMERICSECRET;
+
+        if(acertou){
+            cout << "Parabéns! Você acertou o número secreto!" << endl;
+            error = false;
+        }else if(maior){
+            cout << "Ops! Seu chute foi maior que o número secreto" << endl;
+        }else{
+            cout << "Ops! Seu chute foi menor que o número secreto" << endl;
+        }
     }
+    cout << "Fim de Jogo" << endl;    
+    cout << "Você acertou em "<< tentativas << " tentaivas!" << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuação foi de " << pontos << " pontos!" << endl;
 }
