f5aadb00a417423de912921a95b66f0897e3443e
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-12-09 22:58:53 -0300

    Create game forca

---
+++ JogoDaForca/index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string palavra_secreta = "secreta";
+    cout << palavra_secreta << endl;
+}
