ba2b2c518c1f0a2302351f38e9d11eaebc6d09ee
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-12-15 19:25:23 -0300

    Condição final do jgo

--- JogoDaForca/index.cpp
+++ JogoDaForca/index.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 #include <cstdlib>
-#include <ctime>
 using namespace std;
 
 const string PALAVRASECRETA = "secreta";
+map<char, bool> chutou;
+vector<char> chuteerror;
+
 
 bool letra_existe(char chute){
     /*for(int i = 0; i < PALAVRASECRETA.size(); i++){
@@ -20,20 +24,56 @@ bool letra_existe(char chute){
     return false;
 }
 
+bool naoacertou(){
+    for(char letra : PALAVRASECRETA){
+        if(!chutou[letra]){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool naoenforcou(){
+    return chuteerror.size() < 5;
+}
+
 int main(){
-    cout << PALAVRASECRETA << endl;
+    cout <<"*******************************************" << endl;
+    cout <<"**      Bem vindo ao Jogo da Forca       **" << endl;
+    cout <<"**   Escolha o seu nível de dificuldade  **" << endl;
+    cout <<"**  Fácil (F), Médio (M) ou Difícil (D)  **" << endl;
+    cout <<"*******************************************" << endl;
+    cout << endl;
 
-    bool nao_acertou = true;
-    bool nao_enforcou = true;
+    while(naoacertou() && naoenforcou()){
+        cout << "Chutes errados: ";
+        for(char letra: chuteerror){
+            cout << letra << " ";
+        }
+        cout << endl;
+        
+        for(char letra : PALAVRASECRETA){
+            if(chutou[letra]){
+                cout << letra << " ";
+            }else{
+                cout << "_ ";
+            }
+        }
+        cout << endl;
 
-    while(nao_acertou && nao_enforcou){
+        cout << "Seu chute: " ;
+
         char chute;
         cin >> chute;
+        chutou[chute] = true;
 
         if(letra_existe(chute)){
             cout << "Você acertou! " << endl;
         }else{
             cout << "Você errou! " << endl;
+            chuteerror.push_back(chute);
         }
+
+        cout << endl;
     }
 }
