49368aec879ba504e84f3e878b7bebfc5011f6c7
llarysilva <eduardotecnologo@hotmail.com>
2022-04-12 22:16:25 -0300

    up

---
+++ 2022/VSCode/Forca/le_arquivo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream> 
+#include <vector>
+#include <ctime>
+
+std::vector<std::string>le_arquivo();
---
+++ 2022/VSCode/Forca/salva_arquivo.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+void salva_arquivo(std::vector<std::string> nova_lista);
